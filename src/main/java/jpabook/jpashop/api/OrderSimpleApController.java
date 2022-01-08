60f818a06e2af9c1360284737516e6dc695147c7
IcedAmeri <seokhyunkho@gmail.com>
2022-01-08 14:14:25 +0900

    [API 개발 고급 - 지연 로딩과 조회 성능 최적화] 간단한 주문 조회 V4: JPA에서 DTO로 바로 조회
    쿼리 방식 선택 권장 순서
    1) 우선 엔티티를 DTO로 변환하는 방법 선택
    2) 필요하명 페치 조인으로 성능 최적화 -> 대부분의 성능 이슈 해결
    3) 그래도 안되면 DTO로 직접 조회
    4) 최후의 방법은 JPA 가 제공하는 네이티브 SQL이나 스프링 JDBC Template을 사용하여 SQL을 직접 사용

--- src/main/java/jpabook/jpashop/api/OrderSimpleApController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -66,6 +69,11 @@ public class OrderSimpleApController {
         return result;
     }
 
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
@@ -82,5 +90,4 @@ public class OrderSimpleApController {
             address = order.getDelivery().getAddress(); // LAZY 초기화
         }
     }
-
 }
